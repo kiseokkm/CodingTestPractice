3dcdcf69cacd2844be76789794178915376bcfc6
김기석 <311kskim@gmail.com>
2023-12-18 01:24:49 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    a =""
+    b =""
+    answer = 0
+    for i in num_list:
+        if i %2 == 0:
+            a+=str(i)
+        elif i%2 == 1:
+            b+=str(i)
+    answer = int(a)+int(b)
+    return answer
