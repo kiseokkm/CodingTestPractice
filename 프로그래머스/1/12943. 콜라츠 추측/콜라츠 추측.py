a508f2257e1a0253230d6dbc8ab784b645923fe8
김기석 <311kskim@gmail.com>
2024-05-14 10:08:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,19 +1,11 @@
 def solution(num):
     answer = 0
-    
-    for i in range(500):
+    while num != 1:
         if num % 2 == 0:
-            num = num/2
-            answer += 1
-        if num == 1:
-            break
-        if num % 2 != 0:
-            num = (num*3)+1
-            answer += 1
-        if num == 1:
-            break
-        if answer > 500 and num != 1:
-            answer = -1
-            break
-    
+            num /= 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+        if answer == 500:
+            return -1
     return answer
