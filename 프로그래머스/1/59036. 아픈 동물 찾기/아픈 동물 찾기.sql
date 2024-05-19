c9abf75325a0da26b86828bd1850a3d8361dee3e
김기석 <311kskim@gmail.com>
2024-05-19 22:52:53 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,4 +1,4 @@
-SELECT ANIMAL_ID, NAME 
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick'
-ORDER BY ANIMAL_ID
+SELECT animal_id , name
+from animal_ins
+where Intake_condition like 'sick'
+order by animal_id
