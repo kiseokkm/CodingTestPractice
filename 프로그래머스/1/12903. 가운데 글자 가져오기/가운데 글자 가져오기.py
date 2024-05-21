47240fa3968617a6e12ca522cd2c1536e5935023
김기석 <311kskim@gmail.com>
2024-05-21 14:33:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,6 +1,5 @@
 def solution(s):
-    center = int(len(s)/2)
-    if len(s) % 2 != 0:
-        return s[center]
-    else:
-        return s[center-1:center+1]
+    if len(s) % 2 == 1:
+        return s[ len(s)//2 ]
+    else :
+        return s[ len(s)//2 -1 : len(s)//2 +1 ]
